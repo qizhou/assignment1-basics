c55b9ebac4d89ecd6aa73dde0627af870196a5ed
Qi Zhou <qizhou@ethstorage.io>
2026-04-20 12:18:40 +0800

    add tinystores valid test

--- tests/test_train_bpe_tinystories.py
+++ tests/test_train_bpe_tinystories.py
@@ -1,6 +1,9 @@
-from .adapters import run_train_bpe
+from .adapters import run_train_bpe, get_tokenizer
 from .common import DATA_PATH
+import pickle
+import pytest
 
+@pytest.mark.skip(reason="Too long")
 def test_train_large_bpe_tinystories():
     """
     Ensure that the special tokens are added to the vocabulary and not
@@ -12,3 +15,21 @@ def test_train_large_bpe_tinystories():
         vocab_size=10000,
         special_tokens=["<|endoftext|>"],
     )
+
+
+def test_encdec_tinystores():
+    vocab_path = DATA_PATH / "TinyStoriesV2-GPT4-train.txt.vocab"
+    merge_path = DATA_PATH / "TinyStoriesV2-GPT4-train.txt.merge"
+    valid_path = DATA_PATH / "TinyStoriesV2-GPT4-valid.txt"
+
+    with open(vocab_path, "rb") as f:
+        vocab = pickle.load(f)
+    with open(merge_path, "rb") as f:
+        merges = pickle.load(f)
+    with open(valid_path, "r") as f:
+        valid = f.read()
+
+    tokenizer = get_tokenizer(vocab, merges, ["<|endoftext|>"])
+    ids = tokenizer.encode(valid)
+    output = tokenizer.decode(ids)
+    assert valid == output
